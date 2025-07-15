cd1d246e1a66ae1501ccd87f860715047f430b3a
Hinzeq <whoitex@gmail.com>
2025-07-15 21:22:47 +0200

    refactored

--- src/components/main/NewEntryComponent.vue
+++ src/components/main/NewEntryComponent.vue
@@ -14,11 +14,6 @@
                 </div>
 
                 <div class="mb-5">
-                    <label for="base-input" class="block mb-2 text-sm font-medium text-gray-900">TO DO DATE</label>
-                    <input type="text" id="base-input" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
-                </div>
-
-                <div class="mb-5">
                     <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 ">Status</label>
                     <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
                         <option>send</option>
--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createMemoryHistory, createRouter } from 'vue-router'
+import { createWebHistory, createRouter } from 'vue-router'
 
 import LayoutView from '@/views/LayoutView.vue'
 import NewEntryComponent from '@/components/main/NewEntryComponent.vue'
@@ -22,7 +22,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createMemoryHistory(),
+    history: createWebHistory(),
     routes,
 })
 
